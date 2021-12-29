add9a1b0de9f1ae6217a800b437f9d7b1512b6ba
rodrigoraiman <rodrigoraiman@gmail.com>
2021-12-28 23:55:41 -0300

    referenciando tablas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_020737) do
+ActiveRecord::Schema.define(version: 2021_12_29_025043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_12_29_020737) do
     t.date "event_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id"
+    t.index ["group_id"], name: "index_concerts_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_12_29_020737) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "concerts", "groups"
 end
